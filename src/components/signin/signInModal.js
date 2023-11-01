d381de0070681aee87676bb4b3a492b56a2032a2
유지원 <jna215843@gmail.com>
2023-11-01 20:40:34 +0900

    feat: signin/signup 모달 유효성 검사

--- src/components/signin/signInModal.js
+++ src/components/signin/signInModal.js
@@ -1,7 +1,7 @@
 import { isModalOpen } from "../../actions/loginAction";
 import "./signInModal.css";
 import { useSelector, useDispatch } from "react-redux";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { API_HOST } from "../../constant";
 
@@ -9,14 +9,29 @@ import { API_HOST } from "../../constant";
 export default function LoginModal () {
   const open = useSelector((state) => state.loginReducer.state);
   const dispatch = useDispatch();
-  let submit = false;
-  let isError = [];
 
   const [isSignIn, setIsSignIn] = useState(false);
-  const [form, setForm] = useState({
-    email: '',
-    password: ''
-  });
+  const [submit, setSubmit] = useState(false);
+
+  const [email, setEmail] = useState('');
+  const [emailValid, isEmailValid] = useState(true);
+  const [password, setPassword] = useState('');
+  const [passwordValid, isPasswordValid] = useState(true);
+  const [errMsg, setErrMsg] = useState('');
+
+  useEffect(() => {
+    if (!email.length) {
+      isEmailValid(false);
+    } else {
+      isEmailValid(true);
+    }
+
+    if (!password.length) {
+      isPasswordValid(false);
+    } else {
+      isPasswordValid(true);
+    }
+  }, [email, password]);
 
   /**
    * 로그인, 회원가입 모달 open 여부
@@ -29,6 +44,10 @@ export default function LoginModal () {
    * 로그인,회원가입 페이지 전환
    */
   const onToggleModal = () => {
+    setEmail('');
+    setPassword('');
+    isEmailValid(true);
+    isPasswordValid(true);
     setIsSignIn(!isSignIn);
   }
 
@@ -36,46 +55,48 @@ export default function LoginModal () {
    * 로그인 버튼 눌렀을 때 호출되는 함수
    */
   const onClickSignIn = () => {
-    submit = true;
-    isError = [];
+    setSubmit(true);
 
-    if (errorCheck().length !== 0) {
-      
-      return;
+    if (!email.length) {
+      isEmailValid(false);
+    }
+    if (!password.length) {
+      isPasswordValid(false);
     }
 
-    axios.get(`${API_HOST}/login`)
-    .then(res => {
-      console.log('Success');
-      submit = false;
-    })
-    .catch(err => {
-      console.log(err);
-      submit = false;
-    });
+    // axios.get(`${API_HOST}/signin`)
+    // .then(res => {
+    //   console.log('Success');
+    //   setSubmit(false);
+    // })
+    // .catch(err => {
+    //   console.log(err);
+    //   setSubmit(false);
+    // });
   }
 
   /**
    * 회원가입 버튼 눌렀을 때 호출되는 함수
    */
   const onClickSignUp = () => {
-    //
-  }
+    setSubmit(true);
 
-  /**
-   * 로그인 버튼 눌렀을 때 유효성 검사
-   */
-  const errorCheck = () => {
-    const email = form.email;
-    const password = form.password;
+    if (!email.length) {
+      isEmailValid(false);
+    }
+    if (!password.length) {
+      isPasswordValid(false);
+    }
 
-    if (!email) {
-      isError.push('email is required');
-    }
-    if (!password) {
-      isError.push('password is required');
-    }
-    return isError;
+    // axios.get(`${API_HOST}/signup`)
+    // .then(res => {
+    //   console.log('Success');
+    //   setSubmit(false);
+    // })
+    // .catch(err => {
+    //   console.log(err);
+    //   setSubmit(false);
+    // });
   }
 
   return (
@@ -86,16 +107,26 @@ export default function LoginModal () {
           <div className="signin-modal">
             <h1 className="title">Account Book</h1>
              <form onSubmit={e=> e.preventDefault()}>
-              <input type="email"
-                placeholder="이메일 입력하세요"
-                value={form.email}
-                onChange={e => setForm({...form, email: e.target.value})}
-              ></input>
+              <div>
+                <input type="email"
+                  placeholder="이메일 입력하세요"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                ></input>
+                {
+                  (submit && !emailValid) && <span className="validation">필수값입니다</span>
+                }
+              </div>
+              <div>
               <input type="password"
                 placeholder="비밀번호 입력하세요"
-                value={form.password}
-                onChange={e => setForm({...form, password: e.target.value})}
+                value={password}
+                onChange={e => setPassword(e.target.value)}
               ></input>
+              {
+                (submit && !passwordValid) && <span className="validation">필수값입니다</span>
+              }
+              </div>
               <button onClick={onClickSignIn}>로그인하기</button>
               <div className="social-container">
                 <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
@@ -109,8 +140,18 @@ export default function LoginModal () {
       : <section className="modal-back signup-section">
           <div className="signup-modal">
             <h1 className="title">Account Book</h1>
-            <input type="email" placeholder="이메일 입력하세요"></input>
-            <input type="password" placeholder="비밀번호 입력하세요"></input>
+            <div>
+              <input type="email" placeholder="이메일 입력하세요"></input>
+              {
+                (submit && !emailValid) && <span className="validation">필수값입니다</span>
+              }
+            </div>
+            <div>
+              <input type="password" placeholder="비밀번호 입력하세요"></input>
+              {
+                (submit && !passwordValid) && <span className="validation">필수값입니다</span>
+              }
+            </div>
             <button onClick={onClickSignUp}>회원가입하기</button>
             <div className="social-container">
               <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
