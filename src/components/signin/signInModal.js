01d6b48e7fb8ec5045170f7cfca9d3a29e32a72b
유지원 <jna215843@gmail.com>
2023-11-02 20:04:59 +0900

    fix: signup

--- src/components/signin/signInModal.js
+++ src/components/signin/signInModal.js
@@ -17,6 +17,10 @@ export default function LoginModal () {
   const [emailValid, isEmailValid] = useState(true);
   const [password, setPassword] = useState('');
   const [passwordValid, isPasswordValid] = useState(true);
+  const [username, setUserName] = useState('');
+  const [usernameValid, isUserNameValid] = useState(true);
+  const [nickname, setNickName] = useState('');
+  const [nicknameValid, isNickNameValid] = useState(true);
   const [errMsg, setErrMsg] = useState('');
 
   useEffect(() => {
@@ -31,7 +35,19 @@ export default function LoginModal () {
     } else {
       isPasswordValid(true);
     }
-  }, [email, password]);
+
+    if (!username.length) {
+      isUserNameValid(false);
+    } else {
+      isUserNameValid(true);
+    }
+
+    // if (!nicknameValid.length) {
+    //   isNickNameValid(false);
+    // } else {
+    //   isNickNameValid(true);
+    // }
+  }, [email, password, username, nickname]);
 
   /**
    * 로그인, 회원가입 모달 open 여부
@@ -46,8 +62,12 @@ export default function LoginModal () {
   const onToggleModal = () => {
     setEmail('');
     setPassword('');
+    setUserName('');
+    setNickName('');
     isEmailValid(true);
     isPasswordValid(true);
+    isUserNameValid(true);
+    isNickNameValid(true);
     setIsSignIn(!isSignIn);
   }
 
@@ -64,7 +84,10 @@ export default function LoginModal () {
       isPasswordValid(false);
     }
 
-    axios.post(`https://account-book.store/accounts/login`)
+    axios.post(`https://account-book.store/accounts/login`, {
+      "email": email,
+      "password": password
+    })
     .then(res => {
       console.log('Success');
       setSubmit(false);
@@ -79,6 +102,7 @@ export default function LoginModal () {
    * 회원가입 버튼 눌렀을 때 호출되는 함수
    */
   const onClickSignUp = () => {
+    console.log('onClickSignUp >> ');
     setSubmit(true);
 
     if (!email.length) {
@@ -87,16 +111,34 @@ export default function LoginModal () {
     if (!password.length) {
       isPasswordValid(false);
     }
+    if (!username.length) {
+      isUserNameValid(false);
+    }
+    if (!nickname.length) {
+      isNickNameValid(false);
+    }
+    
+    console.log('isNickNameValid');
+    console.log(nickname);
+    console.log(nicknameValid);
 
-    axios.post("https://account-book.store/accounts/signup")
-    .then(res => {
-      console.log('Success');
-      setSubmit(false);
-    })
-    .catch(err => {
-      console.log(err);
-      setSubmit(false);
-    });
+    if (emailValid && passwordValid && usernameValid && nicknameValid) {
+      axios.post("https://account-book.store/accounts/signup",
+      {
+        "email": email,
+        "password": password,
+        "username": username,
+        "nickname": nickname
+      })
+      .then(res => {
+        console.log('Success');
+        setSubmit(false);
+      })
+      .catch(err => {
+        console.log(err);
+        setSubmit(false);
+      });
+    }
   }
 
   return (
@@ -144,27 +186,27 @@ export default function LoginModal () {
             <h1 className="title">Account Book</h1>
             <form onSubmit={e=> e.preventDefault()}>
               <div>
-                <input type="email" placeholder="이메일 입력하세요"></input>
+                <input type="email" placeholder="이메일 입력하세요" value={email} onChange={e => setEmail(e.target.value)}></input>
                 {
                   (submit && !emailValid) && <span className="validation">필수값입니다</span>
                 }
               </div>
               <div>
-                <input type="password" placeholder="비밀번호 입력하세요"></input>
+                <input type="password" placeholder="비밀번호 입력하세요" value={password} onChange={e => setPassword(e.target.value)}></input>
                 {
                   (submit && !passwordValid) && <span className="validation">필수값입니다</span>
                 }
               </div>
               <div>
-                <input type="text" placeholder="이름을 입력하세요"></input>
+                <input type="text" placeholder="이름을 입력하세요" value={username} onChange={e => setUserName(e.target.value)}></input>
                 {
-                  (submit && !passwordValid) && <span className="validation">필수값입니다</span>
+                  (submit && !usernameValid) && <span className="validation">필수값입니다</span>
                 }
               </div>
               <div>
-                <input type="text" placeholder="닉네임을 입력하세요"></input>
+                <input type="text" placeholder="닉네임을 입력하세요" value={nickname} onChange={e => setNickName(e.target.value)}></input>
                 {
-                  (submit && !passwordValid) && <span className="validation">필수값입니다</span>
+                  (submit && !nicknameValid) && <span className="validation">필수값입니다</span>
                 }
               </div>
               
