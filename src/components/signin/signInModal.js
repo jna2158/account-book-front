6075a38691fa01ae7f6290639b4922a64515f002
유지원 <jna215843@gmail.com>
2023-11-02 20:32:42 +0900

    fix: signup

--- src/components/signin/signInModal.js
+++ src/components/signin/signInModal.js
@@ -123,7 +123,7 @@ export default function LoginModal () {
     console.log(nicknameValid);
 
     if (emailValid && passwordValid && usernameValid && nicknameValid) {
-      axios.post("http://0.0.0.0:8000/accounts/signup",
+      axios.post("http://localhost:8000/accounts/signup",
       {
         "email": email,
         "password": password,
