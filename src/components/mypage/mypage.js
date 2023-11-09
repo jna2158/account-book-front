160b5f85c82dc7058df35700de18cc94771a8eda
유지원 <jna215843@gmail.com>
2023-11-09 22:34:14 +0900

    feat: 로그아웃 완료되었을 때 페이지 새로고침

--- src/components/mypage/mypage.js
+++ src/components/mypage/mypage.js
@@ -65,7 +65,7 @@ export default function Mypage() {
       localStorage.removeItem('ACCESS_TOKEN');
       localStorage.removeItem('user');
       navigate('/');
-      location.reload();
+      window.location.reload();
     })
     .catch(err => {
       throw err
@@ -88,7 +88,7 @@ export default function Mypage() {
       localStorage.removeItem('ACCESS_TOKEN');
       localStorage.removeItem('user');
       navigate('/');
-      location.reload();
+      window.location.reload();
     })
     .catch(err => {
       console.log(err);
