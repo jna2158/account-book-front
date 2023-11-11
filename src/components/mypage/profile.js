d7dc58d7b05560da87a312374ba1065f91ecb6f1
유지원 <jna215843@gmail.com>
2023-11-11 23:18:16 +0900

    feat: mypage css

--- src/components/mypage/profile.js
+++ src/components/mypage/profile.js
@@ -3,6 +3,8 @@ import profile2 from "../../source/account_basic.png";
 import profile3 from "../../source/account_dog.png";
 import profile4 from "../../source/account_man.png";
 import profile5 from "../../source/account_seal.png";
+import profile6 from "../../source/account_girl.png";
+import profile7 from "../../source/account_soon.png";
 import "./profile.css";
 
 export default function Profile() {
@@ -13,6 +15,8 @@ export default function Profile() {
       <img src={profile3}></img>
       <img src={profile4}></img>
       <img src={profile5}></img>
+      <img src={profile6}></img>
+      <img src={profile7}></img>
     </section>
   )
 }
