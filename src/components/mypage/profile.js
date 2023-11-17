92f7e0877759370f052af6007235e4bd04872a6b
유지원 <jna215843@gmail.com>
2023-11-17 16:59:23 +0900

    feat: mypage profile

--- src/components/mypage/profile.js
+++ src/components/mypage/profile.js
@@ -14,7 +14,8 @@ export default function Profile({setProfile, setIsClickProfieBtn}) {
     setProfile(profile)
     setIsClickProfieBtn(false);
   }
-  const imageList = ['account_baby'];
+  const [isLoading, setIsLoading] = useState(false);
+
   const accessKeyId = process.env.REACT_APP_ACCESS_KEY_ID;
   const secretAccessKey = process.env.REACT_APP_SECRET_ACCESS_KEY;
   const bucketName = process.env.REACT_APP_BUCKET_NAME;
@@ -27,16 +28,18 @@ export default function Profile({setProfile, setIsClickProfieBtn}) {
     },
     region,
   });
-  const [imageUrl, setImageUrl] = useState('');
+  const [imageUrl, setImageUrl] = useState([]);
 
   useEffect(() => {
     // S3에서 이미지 가져오기
     const getImageFromS3 = async () => {
-      try {
-        imageList.forEach(async (image) => {
+      setIsLoading(false);
+      let idx = 0;
+      while(1) {
+        try {
           const params = {
             Bucket: bucketName,
-            Key: `images/basic_profile/${image}.png`, // 이미지 파일의 키
+            Key: `images/basic_profile/account_profile_${idx}.png`, // 이미지 파일의 키
           };
   
           const response = await s3.getObject(params).promise();
@@ -47,11 +50,14 @@ export default function Profile({setProfile, setIsClickProfieBtn}) {
           // Blob을 이용하여 이미지 URL 생성
           const url = URL.createObjectURL(blob);
   
-          // 이미지 URL 상태 업데이트
-          setImageUrl(url);
-        });
-      } catch (error) {
-        console.error('Error fetching image from S3:', error);
+          const newUrlArr = imageUrl;
+          newUrlArr.push(url);
+          setImageUrl(newUrlArr);
+          idx++;
+        } catch (error) {
+          setIsLoading(true);
+          break;
+        }
       }
     };
 
@@ -59,14 +65,16 @@ export default function Profile({setProfile, setIsClickProfieBtn}) {
   }, []);
 
   return(
-    <section className="profile_section">
-      <img src={imageUrl} onClick={() =>handleClickProfile(profile1)}></img>
-      {/* <img src={profile2} onClick={() =>handleClickProfile(profile2)}></img>
-      <img src={profile3} onClick={() =>handleClickProfile(profile3)}></img>
-      <img src={profile4} onClick={() =>handleClickProfile(profile4)}></img>
-      <img src={profile5} onClick={() =>handleClickProfile(profile5)}></img>
-      <img src={profile6} onClick={() =>handleClickProfile(profile6)}></img>
-      <img src={profile7} onClick={() =>handleClickProfile(profile7)}></img> */}
-    </section>
+    isLoading && (
+      <section className="profile_section">
+        <img src={imageUrl[0]} onClick={() =>handleClickProfile(profile1)}></img>
+        <img src={imageUrl[1]} onClick={() =>handleClickProfile(profile2)}></img>
+        <img src={imageUrl[2]} onClick={() =>handleClickProfile(profile3)}></img>
+        <img src={imageUrl[3]} onClick={() =>handleClickProfile(profile4)}></img>
+        <img src={imageUrl[4]} onClick={() =>handleClickProfile(profile5)}></img>
+        <img src={imageUrl[5]} onClick={() =>handleClickProfile(profile6)}></img>
+        <img src={imageUrl[6]} onClick={() =>handleClickProfile(profile7)}></img>
+      </section>
+    )
   )
 }
