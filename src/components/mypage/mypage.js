6efc688efff592f29571bcce75494606066d511b
유지원 <jna215843@gmail.com>
2023-11-17 19:12:46 +0900

    refactor: 프로필 이미지 가져오기 비동기 처리

--- src/components/mypage/mypage.js
+++ src/components/mypage/mypage.js
@@ -5,6 +5,7 @@ import Profile from './profile';
 import './mypage.css';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { S3 } from "aws-sdk";
 import { signInErrorMsg, signUpErrorMsg } from '../../shared/errorMsgConstant';
 
 
@@ -25,9 +26,23 @@ export default function Mypage() {
 
   const navigate = useNavigate();
 
+  // s3
+  const accessKeyId = process.env.REACT_APP_ACCESS_KEY_ID;
+  const secretAccessKey = process.env.REACT_APP_SECRET_ACCESS_KEY;
+  const bucketName = process.env.REACT_APP_BUCKET_NAME;
+  const region = process.env.REACT_APP_REGION;
+  const s3 = new S3({
+    credentials: {
+      accessKeyId,
+      secretAccessKey,
+    },
+    region,
+  });
+  const [profileList, setProfileList] = useState([]);
+
   useEffect(() => {
     getUserInfo();
-    getProfileList();
+    getImageFromS3();
   }, []);
 
   /** 마이페이지 정보 get */
@@ -52,6 +67,38 @@ export default function Mypage() {
       console.log(err);
     })
   }
+
+  /** 프로필 리스트 get */
+  const getImageFromS3 = async () => {
+    let idx = 0;
+    while(1) {
+      try {
+        const params = {
+          Bucket: bucketName,
+          Key: `images/basic_profile/account_profile_${idx}.png`,
+        };
+
+        // 이미지를 요청해서 가져오기
+        // await: 비동기 함수에서 결과를 기다림
+        // promise(): 비동기 작업이 완료될 때까지 기다림
+        const response = await s3.getObject(params).promise();
+
+        // 이미지 데이터를 Blob으로 변환
+        // s3에서 가져온 이미지 데이터를 브라우저에서 표시할 수 있는 형식으로 변환함
+        const blob = new Blob([response.Body], { type: response.ContentType });
+
+        // Blob을 이용하여 이미지 URL 생성
+        const url = URL.createObjectURL(blob);
+
+        const newUrlArr = profileList;
+        newUrlArr.push(url);
+        setProfileList(newUrlArr);
+        idx++;
+      } catch (error) {
+        break;
+      }
+    }
+  };
 
   /** 탈퇴 요청 */
   const deleteUser = () => {
@@ -98,7 +145,7 @@ export default function Mypage() {
       delete formData.password_check;
     }
 
-    formData.imageUrl = profile;
+    formData.profileList = profile;
     if (formData.nickname === originForm.nickname) {
       delete formData.nickname;
     }
@@ -147,7 +194,7 @@ export default function Mypage() {
           <img className="profile" src={profile}></img>
         </div>
         {
-          isClickProfileBtn ? <Profile setProfile={setProfile} setIsClickProfieBtn={setIsClickProfieBtn}/> : ''
+          isClickProfileBtn ? <Profile profileList={profileList} setProfile={setProfile} setIsClickProfieBtn={setIsClickProfieBtn}/> : ''
         }
 
         <div>
--- src/components/mypage/profile.js
+++ src/components/mypage/profile.js
@@ -1,77 +1,20 @@
 import "./profile.css";
-import { S3 } from "aws-sdk";
-import { useEffect, useState } from "react";
 
-export default function Profile({setProfile, setIsClickProfieBtn}) {
+export default function Profile({profileList, setProfile, setIsClickProfieBtn}) {
   const handleClickProfile = (profile) => {
     setProfile(profile)
     setIsClickProfieBtn(false);
   }
-  const [isLoading, setIsLoading] = useState(false);
-
-  const accessKeyId = process.env.REACT_APP_ACCESS_KEY_ID;
-  const secretAccessKey = process.env.REACT_APP_SECRET_ACCESS_KEY;
-  const bucketName = process.env.REACT_APP_BUCKET_NAME;
-  const region = process.env.REACT_APP_REGION;
-
-  const s3 = new S3({
-    credentials: {
-      accessKeyId,
-      secretAccessKey,
-    },
-    region,
-  });
-  const [imageUrl, setImageUrl] = useState([]);
-
-  useEffect(() => {
-    // S3에서 이미지 가져오기
-    const getImageFromS3 = async () => {
-      setIsLoading(false);
-      let idx = 0;
-      while(1) {
-        try {
-          const params = {
-            Bucket: bucketName,
-            Key: `images/basic_profile/account_profile_${idx}.png`,
-          };
-
-          // 이미지를 요청해서 가져오기
-          // await: 비동기 함수에서 결과를 기다림
-          // promise(): 비동기 작업이 완료될 때까지 기다림
-          const response = await s3.getObject(params).promise();
-  
-          // 이미지 데이터를 Blob으로 변환
-          // s3에서 가져온 이미지 데이터를 브라우저에서 표시할 수 있는 형식으로 변환함
-          const blob = new Blob([response.Body], { type: response.ContentType });
-  
-          // Blob을 이용하여 이미지 URL 생성
-          const url = URL.createObjectURL(blob);
-  
-          const newUrlArr = imageUrl;
-          newUrlArr.push(url);
-          setImageUrl(newUrlArr);
-          idx++;
-        } catch (error) {
-          setIsLoading(true);
-          break;
-        }
-      }
-    };
-
-    getImageFromS3();
-  }, []);
 
   return(
-    isLoading && (
-      <section className="profile_section">
-        <img src={imageUrl[0]} onClick={() =>handleClickProfile(imageUrl[0])}></img>
-        <img src={imageUrl[1]} onClick={() =>handleClickProfile(imageUrl[1])}></img>
-        <img src={imageUrl[2]} onClick={() =>handleClickProfile(imageUrl[2])}></img>
-        <img src={imageUrl[3]} onClick={() =>handleClickProfile(imageUrl[3])}></img>
-        <img src={imageUrl[4]} onClick={() =>handleClickProfile(imageUrl[4])}></img>
-        <img src={imageUrl[5]} onClick={() =>handleClickProfile(imageUrl[5])}></img>
-        <img src={imageUrl[6]} onClick={() =>handleClickProfile(imageUrl[6])}></img>
-      </section>
-    )
+    <section className="profile_section">
+      <img src={profileList[0]} onClick={() =>handleClickProfile(profileList[0])}></img>
+      <img src={profileList[1]} onClick={() =>handleClickProfile(profileList[1])}></img>
+      <img src={profileList[2]} onClick={() =>handleClickProfile(profileList[2])}></img>
+      <img src={profileList[3]} onClick={() =>handleClickProfile(profileList[3])}></img>
+      <img src={profileList[4]} onClick={() =>handleClickProfile(profileList[4])}></img>
+      <img src={profileList[5]} onClick={() =>handleClickProfile(profileList[5])}></img>
+      <img src={profileList[6]} onClick={() =>handleClickProfile(profileList[6])}></img>
+    </section>
   )
 }
