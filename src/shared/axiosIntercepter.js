cd106a067565cb2c13a1b651b801c3d6b66d2a30
유지원 <jna215843@gmail.com>
2023-12-03 13:11:45 +0900

    fix: refreshToken test

--- src/shared/axiosIntercepter.js
+++ src/shared/axiosIntercepter.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { updateRefreshToken } from './token';
 
 // 요청 인터셉터 설정
 axios.interceptors.request.use(
@@ -21,22 +20,39 @@ axios.interceptors.response.use(
     return response;
   },
   async (error) => {
-    if (error.response && error.response.status === 401) {
-      const originalRequest = error.config;
-      return updateRefreshToken()
-        .then((res) => {
-          console.log('res');
-          console.log(res);
-          if (res === 'success') {
-            return axios.request(originalRequest);
-          } else {
-            return Promise.reject(error);
-          }
-        })
-        .catch((err) => {
-          return Promise.reject(error);
-        });
+    if (error.response && error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true; // 중복 요청 방지
+      return retryOriginalRequest(error, originalRequest);
     }
     return Promise.reject(error);
   }
 );
+
+const retryOriginalRequest = async (error, refreshToken) => {
+  try {
+    const originalRequest = error.config;
+    const res = await updateRefreshToken();
+
+    if (res === 'success') {
+      return axios(originalRequest);
+    } else {
+      return Promise.reject(error);
+    }
+  } catch (refreshError) {
+    console.error('Error during refresh token:', refreshError);
+    return Promise.reject(error);
+  }
+};
+
+
+const updateRefreshToken = async () => {
+  axios.post(`${API_HOST}/api/accounts/login/refresh/`)
+  .then(res => {
+    localStorage.setItem('ACCESS_TOKEN', res.data.access_token);
+    return 'success';
+  })
+  .catch(err => {
+    console.log(err);
+    return 'fail';
+  })
+}
