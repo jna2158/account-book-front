f16071d0396155d57698167504cd9167baea4fc0
유지원 <jna215843@gmail.com>
2023-12-08 19:47:09 +0900

    feat: chart

--- src/components/chart/chartGraph/GraphTopTags.js
+++ src/components/chart/chartGraph/GraphTopTags.js
@@ -1,4 +1,4 @@
-import { ResponsivePie } from '@nivo/pie';
+import { ResponsiveBar } from '@nivo/bar'
 import { useEffect, useState } from 'react';
 
 export default function GraphTopTags({ data }) {
@@ -8,28 +8,31 @@ export default function GraphTopTags({ data }) {
   const [loading, setLoding] = useState(false);
 	const [graphType, setGraphType] = useState('income_top_ten');
 
-  /* 월 단위 Tag top ten % data format */
-  const topPercentageFormat = () => {
+  /* 월 단위 Tag top tags format */
+  const topTagFormat = () => {
     setLoding(true);
-    const spendingArray = Object.entries(data.spending_top_ten).map(([id, value], index) => ({
-      id,
-      label: id,
-      value,
-      color: `hsl(191, 70%, 50%)`
-    }));
-  
-    const incomeArray = Object.entries(data.income_top_ten).map(([id, value], index) => ({
-      id,
-      label: id,
-      value,
-      color: `hsl(191, 70%, 50%)`
-    }));
-    
-    // 결과 객체 생성
-    const resultObject = {
+
+		if (!data) {
+			return;
+		}
+		const spendingArray = Object.keys(data.spending_top_ten).map(key => ({
+			content: key,
+			pay: Number(data.spending_top_ten[key]),
+			color: "hsl(67, 70%, 50%)"
+		}));
+
+		const incomeArray = Object.keys(data.income_top_ten).map(key => ({
+			content: key,
+			pay: Number(data.income_top_ten[key]),
+			color: "hsl(67, 70%, 50%)"
+		}));
+
+		const resultObject = {
       income_top_ten: incomeArray,
       spending_top_ten: spendingArray
     };
+		console.log('resultObject >> ');
+		console.log(resultObject);
     setList(resultObject);
     setLoding(true);
   }
@@ -37,8 +40,8 @@ export default function GraphTopTags({ data }) {
   useEffect(() => {
     setLoding(true);
     setTimeout(() => {
-      topPercentageFormat();
-    }, []);
+      topTagFormat();
+    }, 1000);
   }, []);
 
   return (
@@ -67,132 +70,104 @@ export default function GraphTopTags({ data }) {
 				list[`${graphType}`] && list[`${graphType}`].length
 				? (
 					loading && (
-						<ResponsivePie
-							data={list[`${graphType}`]}
-							margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-							innerRadius={0.5}
-							padAngle={0.7}
-							cornerRadius={3}
-							activeOuterRadiusOffset={8}
-							borderWidth={1}
-							borderColor={{
-									from: 'color',
-									modifiers: [
-											[
-													'darker',
-													0.2
-											]
-									]
-							}}
-							arcLinkLabelsSkipAngle={10}
-							arcLinkLabelsTextColor="#333333"
-							arcLinkLabelsThickness={2}
-							arcLinkLabelsColor={{ from: 'color' }}
-							arcLabelsSkipAngle={10}
-							arcLabelsTextColor={{
-									from: 'color',
-									modifiers: [
-											[
-													'darker',
-													2
-											]
-									]
-							}}
-							defs={[
-									{
-											id: 'dots',
-											type: 'patternDots',
-											background: 'inherit',
-											color: 'rgba(255, 255, 255, 0.3)',
-											size: 4,
-											padding: 1,
-											stagger: true
-									},
-									{
-											id: 'lines',
-											type: 'patternLines',
-											background: 'inherit',
-											color: 'rgba(255, 255, 255, 0.3)',
-											rotation: -45,
-											lineWidth: 6,
-											spacing: 10
-									}
-							]}
-							fill={[
-									{
-											match: {
-													id: 'ruby'
-											},
-											id: 'dots'
-									},
-									{
-											match: {
-													id: 'c'
-											},
-											id: 'dots'
-									},
-									{
-											match: {
-													id: 'go'
-											},
-											id: 'dots'
-									},
-									{
-											match: {
-													id: 'python'
-											},
-											id: 'dots'
-									},
-									{
-											match: {
-													id: 'scala'
-											},
-											id: 'lines'
-									},
-									{
-											match: {
-													id: 'lisp'
-											},
-											id: 'lines'
-									},
-									{
-											match: {
-													id: 'elixir'
-											},
-											id: 'lines'
-									},
-									{
-											match: {
-													id: 'javascript'
-											},
-											id: 'lines'
-									}
-							]}
-							legends={[
-									{
-											anchor: 'bottom',
-											direction: 'row',
-											justify: false,
-											translateX: 0,
-											translateY: 56,
-											itemsSpacing: 0,
-											itemWidth: 100,
-											itemHeight: 18,
-											itemTextColor: '#999',
-											itemDirection: 'left-to-right',
-											itemOpacity: 1,
-											symbolSize: 18,
-											symbolShape: 'circle',
-											effects: [
-													{
-															on: 'hover',
-															style: {
-																	itemTextColor: '#000'
-															}
-													}
-											]
-									}
-							]}
+						<ResponsiveBar
+								data={list.spending_top_ten}
+								keys={[
+										'pay'
+								]}
+								indexBy="content"
+								margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+								padding={0.3}
+								valueScale={{ type: 'linear' }}
+								indexScale={{ type: 'band', round: true }}
+								colors={{ scheme: 'nivo' }}
+								defs={[
+										{
+												id: 'dots',
+												type: 'patternDots',
+												background: 'inherit',
+												color: '#38bcb2',
+												size: 4,
+												padding: 1,
+												stagger: true
+										},
+										{
+												id: 'lines',
+												type: 'patternLines',
+												background: 'inherit',
+												color: '#eed312',
+												rotation: -45,
+												lineWidth: 6,
+												spacing: 10
+										}
+								]}
+								borderColor={{
+										from: 'color',
+										modifiers: [
+												[
+														'darker',
+														1.6
+												]
+										]
+								}}
+								axisTop={null}
+								axisRight={null}
+								axisBottom={{
+										tickSize: 5,
+										tickPadding: 5,
+										tickRotation: 0,
+										legend: 'content',
+										legendPosition: 'middle',
+										legendOffset: 32,
+										truncateTickAt: 0
+								}}
+								axisLeft={{
+										tickSize: 5,
+										tickPadding: 5,
+										tickRotation: 0,
+										legend: 'pay',
+										legendPosition: 'middle',
+										legendOffset: -40,
+										truncateTickAt: 0
+								}}
+								labelSkipWidth={12}
+								labelSkipHeight={12}
+								labelTextColor={{
+										from: 'color',
+										modifiers: [
+												[
+														'darker',
+														1.6
+												]
+										]
+								}}
+								legends={[
+										{
+												dataFrom: 'keys',
+												anchor: 'bottom-right',
+												direction: 'column',
+												justify: false,
+												translateX: 120,
+												translateY: 0,
+												itemsSpacing: 2,
+												itemWidth: 100,
+												itemHeight: 20,
+												itemDirection: 'left-to-right',
+												itemOpacity: 0.85,
+												symbolSize: 20,
+												effects: [
+														{
+																on: 'hover',
+																style: {
+																		itemOpacity: 1
+																}
+														}
+												]
+										}
+								]}
+								role="application"
+								ariaLabel="Nivo bar chart demo"
+								barAriaLabel={e=>e.id+": "+e.formattedValue+" in content: "+e.indexValue}
 						/>
 					)
 				)
