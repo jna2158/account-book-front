6c23faad3d8a9fa21d640653bd53a77f1ec6b81c
유지원 <jna215843@gmail.com>
2023-12-17 22:55:08 +0900

    refactor: chart

--- src/components/toast/toast.js
+++ src/components/toast/toast.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import './toast.css'; // 스타일링 파일을 따로 생성
 
-const Toast = ({ message, duration = 3000, onClose }) => {
+const Toast = ({ message, onClose }) => {
   const [visible, setVisible] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setVisible(false);
-      onClose && onClose();
-    }, duration);
+      onClose();
+    }, 3000);
 
     return () => clearTimeout(timer);
-  }, [duration, onClose]);
+  }, [3000, onClose]);
 
   return visible ? (
     <div className="toast-container">
